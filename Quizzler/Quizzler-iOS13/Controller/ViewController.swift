2a3c97c9076af3c31b6773e549e07c7ed1e3de15
AashiqMortimer <111452244+AashiqMortimer@users.noreply.github.com>
2022-10-11 12:16:48 +0100

    Add files via upload

---
+++ Quizzler/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad() 
+        updateUI()
+        progressBar.progress = 0.0
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            //print("Right!")
+            sender.backgroundColor = UIColor.green
+        } else {
+            //print("Wrong!")
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Question: \(quizBrain.questionNumber + 1) of \(quizBrain.quiz.count)\nScore: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+    
+}
